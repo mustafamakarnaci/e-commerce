2722207e49c7f2378b4606681ee4445f3584737e
Mustafa MAKARNACI <mustafazymakarnaci@gmail.com>
2021-05-01 02:11:26 +0300

    added card component to display products

--- ecommerce-front/src/core/Home.js
+++ ecommerce-front/src/core/Home.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from './Layout'
+import { getProducts } from './apiCore';
 
 const Home = () => {
+
+    const [productsBySell, setProductsBySell] = useState([]);
+    const [productsByArrival, setProductsByArrival] = useState([])
+    const [error, setError] = useState(false);
+
+    const loadProductsBySell = () => {
+        getProducts('sold').then(data => {
+            if(data.error){
+                setError(true)
+            }else{
+                setProductsBySell(data);
+            }
+        })
+    }
+    const loadProductsByArrival = () => {
+        getProducts('createdAt').then(data => {
+            if(data.error){
+                setError(true)
+            }else{
+                setProductsByArrival(data);
+            }
+        })
+    }
+
+
+    useEffect(() => {
+        loadProductsByArrival()
+        loadProductsBySell()
+        
+    }, [])
+
     return (
         <Layout title='Home Page' description="Node React E-Commerce App">
-            ...
+            {JSON.stringify(productsBySell)}
+            <hr/>
+            {JSON.stringify(productsByArrival)}
         </Layout>
     )
 }
---
+++ ecommerce-front/src/core/apiCore.js
@@ -0,0 +1,16 @@
+import { API } from '../config';
+
+
+/**
+ * getCategories fetch
+ */
+
+ export const getProducts = (sortBy) => {
+    return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json()
+        })
+        .catch(err => console.log(err));
+}
